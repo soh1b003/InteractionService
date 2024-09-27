1b40cc3ae5bbe8ed5707c584b1b9fff2c2d6195c
soh1b003 <sohibjinnorboboyev@gmail.com>
2024-09-27 11:46:40 +0500

    Initial commit

---
+++ src/test/java/org/example/interactionservice/InteractionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.interactionservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InteractionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
